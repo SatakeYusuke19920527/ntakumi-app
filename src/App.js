dfb06f1647331d92406f77c765d3d3182fab0784
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-12-27 13:54:29 +0900

    ウルトラ進化

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App () {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Takumi satake
+          Takumi satake yusuke
         </a>
       </header>
     </div>
