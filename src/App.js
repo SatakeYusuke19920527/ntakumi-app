608a7b19368976cabd538adb4a3331a227b5bf45
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-27 14:12:13 +0900

    あとはよろしく

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App () {
         >
           Nishimura
         </a>
-        <Sensei />
       </header>
     </div>
   )
--- src/Sensei.js
+++ src/Sensei.js
@@ -1,7 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Sensei = () => {
   const [te, setTe] = useState('')
+  const [cpuTe, setCpuTe] = useState('')
+  
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 3)
+    console.log(random, 'random')
+    if (random === 0) {
+      setCpuTe("ぐー")  
+    } else if (random === 1) {
+      setCpuTe("ちょき")
+    }else if (random === 2) {
+      setCpuTe("ぱー")
+    } else {
+      window.alert('error')
+    }
+  }, [te])
+
   const gu = () => {
     setTe('ぐー')
   }
@@ -17,6 +33,7 @@ const Sensei = () => {
       <button onClick={tyoki}>ちょき</button>
       <button onClick={pa}>ぱー</button>
       <h1>あなたの手: {te}</h1>
+      <h1>CPUの手: {cpuTe}</h1>
     </div>
   )
 }
