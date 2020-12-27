3fd70c71a44b1bc28cb0d015b97c808a67241f19
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-12-27 13:52:59 +0900

    超進化

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App () {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Takumi satake
+          Nishimura
         </a>
       </header>
     </div>
